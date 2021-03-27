cec8d050d7aebec0b190a90948ffaa5af2b2714c
grejioh <grejioh@outlook.com>
2021-03-27 13:46:03 +0000

    can print the same bitmap, but not perfect

--- lab3/zoom.c
+++ lab3/zoom.c
@@ -40,12 +40,19 @@ void copyBmpFile(char *sourceName, char *targetName) {
 
   fread(&sourceFileHeader, sizeof(bmpFileHeader), 1, src);
   fread(&sourceInfoHeader, sizeof(bmpInfoHeader), 1, src);
-
+  if (sourceFileHeader.bfType != 0x4D42) {
+    fclose(src);
+    printf("%x FUCK oOFF", sourceFileHeader.bfType);
+    return;
+  }
   int size = sourceInfoHeader.biHeight * sourceInfoHeader.biWidth;
+  int bytesPerLine =
+      ((sourceInfoHeader.biWidth * sourceInfoHeader.biBitCount + 31) >> 5) << 2;
+  size = bytesPerLine * sourceInfoHeader.biHeight;
   printf("%d\n%d", sourceInfoHeader.biHeight, sourceInfoHeader.biWidth);
 
   sourceFilePixels = (unsigned int *)malloc(sizeof(unsigned int) * size);
-
+  fread(sourceFilePixels, sizeof(int), size, src);
   int height = (int)(sourceInfoHeader.biHeight);
   int width = (int)(sourceInfoHeader.biWidth);
   int newsize = height * width;
@@ -54,29 +61,30 @@ void copyBmpFile(char *sourceName, char *targetName) {
 
   targetFileHeader = sourceFileHeader;
   targetInfoHeader = sourceInfoHeader;
+  targetFilePixels = sourceFilePixels;
+  /*  */
+  /* for (int i = 0; i < height; i++) { */
+  /*   for (int j = 0; j < width; j++) { */
+  /*     int m = (int)sourceInfoHeader.biHeight + j; */
+  /*     targetFilePixels[i * height + j] = sourceFilePixels[m]; */
+  /*   } */
+  /* } */
 
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      int m = (int)sourceInfoHeader.biHeight + j;
-      targetFilePixels[i * height + j] = sourceFilePixels[m];
-    }
-  }
+  fwrite(&sourceFileHeader, sizeof(bmpFileHeader), 1, tar);
+  fwrite(&sourceInfoHeader, sizeof(bmpInfoHeader), 1, tar);
 
-  fwrite(&targetFileHeader, sizeof(bmpFileHeader), 1, tar);
-  fwrite(&targetInfoHeader, sizeof(bmpInfoHeader), 1, tar);
-
-  fwrite(targetFilePixels, newsize, sizeof(int), tar);
+  fwrite(sourceFilePixels, sizeof(int), size, tar);
   fclose(src);
-  /* fclose(tar); */
+  fclose(tar);
 }
 
 void testFunc() {
   /* printf("%lu\n", sizeof(sourceFileHeader)); */
   /* printf("%lu\n", sizeof(bmpInfoHeader)); */
-  /* printf("%lu\n", sizeof(sourceFileHeader.bfReserved2)); */
+  printf("%u\n", sourceInfoHeader.biBitCount);
 }
 int main(int argc, char *argv[]) {
-  testFunc();
   copyBmpFile(argv[1], argv[2]);
+  testFunc();
   return 0;
 }
