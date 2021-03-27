58a11ce2865c2b6a3f97028940e052c18896591f
grejioh <grejioh@outlook.com>
2021-03-27 12:13:10 +0000

    no error

--- lab3/zoom.c
+++ lab3/zoom.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#pragma pack(1)
 typedef struct {
   unsigned short int bfType;
   unsigned int bfSize;
@@ -32,17 +33,50 @@ void copyBmpFile(char *sourceName, char *targetName) {
     return;
   }
 
+  if (tar == NULL) {
+    printf("Somethings wrong!!");
+    return;
+  }
+
   fread(&sourceFileHeader, sizeof(bmpFileHeader), 1, src);
   fread(&sourceInfoHeader, sizeof(bmpInfoHeader), 1, src);
+
+  int size = sourceInfoHeader.biHeight * sourceInfoHeader.biWidth;
+  printf("%d\n%d", sourceInfoHeader.biHeight, sourceInfoHeader.biWidth);
+
+  sourceFilePixels = (unsigned int *)malloc(sizeof(unsigned int) * size);
+
+  int height = (int)(sourceInfoHeader.biHeight);
+  int width = (int)(sourceInfoHeader.biWidth);
+  int newsize = height * width;
+
+  targetFilePixels = (unsigned int *)malloc(sizeof(unsigned int) * newsize);
+
+  targetFileHeader = sourceFileHeader;
+  targetInfoHeader = sourceInfoHeader;
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      int m = (int)sourceInfoHeader.biHeight + j;
+      targetFilePixels[i * height + j] = sourceFilePixels[m];
+    }
+  }
+
+  fwrite(&targetFileHeader, sizeof(bmpFileHeader), 1, tar);
+  fwrite(&targetInfoHeader, sizeof(bmpInfoHeader), 1, tar);
+
+  fwrite(targetFilePixels, newsize, sizeof(int), tar);
+  fclose(src);
+  /* fclose(tar); */
 }
 
 void testFunc() {
-  printf("%lu\n", sizeof(sourceFileHeader));
-  printf("%lu\n", sizeof(bmpInfoHeader));
-  printf("%lu\n", sizeof(sourceFileHeader.bfReserved2));
+  /* printf("%lu\n", sizeof(sourceFileHeader)); */
+  /* printf("%lu\n", sizeof(bmpInfoHeader)); */
+  /* printf("%lu\n", sizeof(sourceFileHeader.bfReserved2)); */
 }
 int main(int argc, char *argv[]) {
   testFunc();
-  copyBmpFile(argv[1], argv[3]);
+  copyBmpFile(argv[1], argv[2]);
   return 0;
 }
